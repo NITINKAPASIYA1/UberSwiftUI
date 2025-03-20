f01f2ec815a19892dfbc90369635088268a2f1d0
Nitin Kumar <kumarnitin5814@gmail.com>
2025-03-21 01:06:44 +0530

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Nitin on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
